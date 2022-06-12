5e9d5bfa432a8432a63fa274f7116ae82969bf5a
Vilian P <vilkata41@gmail.com>
2022-06-12 15:08:50 +0300

    Fixing parsing and adding match tips

--- LSbot.py
+++ LSbot.py
@@ -25,10 +25,14 @@ async def on_message(msg):
                           "'ls change_region (regionName)' where (regionName) is one of the " \
                           "following: BR, EUNE, EUW, JP, KR, LAN, LAS, NA, OCE, RU, TR." \
                           "Right now you can check the following commands: \n\n" \
-                          "-ls tips_with (championName) - where (championName) should be replaced" \
-                          "with the champion name you want to check tips for gameplay of.\n\n" \
-                          "-ls tips_against (championName) - where (championName) should be replaced" \
-                          "with the champion you are playing against."
+                          "-ls tips_with '(championName)' - where (championName) should be replaced" \
+                          "with the champion name you want to check tips for gameplay of. Also, please" \
+                          "don't miss the single quotes. \n\n" \
+                          "-ls tips_against '(championName)' - where (championName) should be replaced" \
+                          "with the champion you are playing against. Also, please don't miss the single quotes.\n\n" \
+                          "-ls match_tips '(summonerName)' - where (summonerName) should be replaced" \
+                          "with the summoner name you want to check the live game of. Also, please don't miss" \
+                          "the single quotes. \n\n"
             await msg.channel.send(message)
         elif cmd[0] == "change_region":
             if len(cmd) != 2:
@@ -49,12 +53,12 @@ async def on_message(msg):
             else:
                 message = apiTesting.tips_against_champ(cmd[1], curr_region)
             await msg.channel.send(message)
-        elif cmd[0] == "matchup_tips":
+        elif cmd[0] == "match_tips":
             await msg.channel.send("Let me datamine. Give me one moment...")
             if len(cmd) != 2:
                 message = "Wrong number of arguments. Check 'ls help' to see the commands."
             else:
-                message = apiTesting.get_matchup_info(cmd[1], curr_region)
+                message = apiTesting.get_match_tips(cmd[1], curr_region)
             await msg.channel.send(message)
         else:
             await msg.channel.send("Sorry, my parents didn't teach me how to answer to this. "
@@ -66,7 +70,23 @@ def parse(inpt):
         return []
 
     result_string = inpt.replace("ls ", "", 1)
-    result_list = result_string.split(" ")
+    split_list = result_string.split(" ")
+    result_list = []
+    sp_counter = 0
+
+    while sp_counter < len(split_list):
+        if split_list[sp_counter].startswith("'"):
+            item = ""
+            while not split_list[sp_counter].endswith("'"):
+                item += split_list[sp_counter] + " "
+                sp_counter += 1
+            item += split_list[sp_counter]
+            sp_counter += 1
+            result_list.append(item.strip("'"))
+        else:
+            result_list.append(split_list[sp_counter])
+            sp_counter += 1
+
     return result_list
 
 
--- apiTesting.py
+++ apiTesting.py
@@ -33,14 +33,25 @@ def tips_against_champ(champ_name, region):
         return "Please, enter a valid champion name in League of Legends."
 
 
-def get_matchup_info(summoner_name, curr_region):
+def get_match_tips(summoner_name, curr_region):
     allowed_queues = ["NORMAL_5x5_DRAFT", "RANKED_FLEX_SR", "RANKED_FLEX_TT", "RANKED_SOLO_5x5"]
-    curr_summoner = cass.Summoner(name=summoner_name, region=curr_region)
-    curr_match = curr_summoner.current_match
-    curr_champion = curr_match.participants[summoner_name].champion.name
-
-    if curr_match.queue not in allowed_queues:
+    curr_match = cass.Summoner(name=summoner_name, region=curr_region).current_match
+    if curr_match.queue.value not in allowed_queues:
         return "This queue type is not supported. By LolStatistico. The allowed queues are: " \
                "Normal Draft, Flex, Solo Queue."
 
-    return ""
+    result = ""
+    curr_participant = curr_match.participants[summoner_name]
+    curr_champion = curr_participant.champion.name
+    result += f"Tips to play as {curr_champion}: \n" + tips_with_champ(curr_champion, curr_region)
+
+    curr_side = curr_participant.side.value
+    enemy_team = curr_match.blue_team if curr_side == 200 else curr_match.red_team
+
+    for p in enemy_team.participants:
+        enemy_champ = p.champion.name
+        result += "\n"
+        result += f"Tips for playing against {enemy_champ}:\n"
+        result += tips_against_champ(enemy_champ, curr_region)
+
+    return result
